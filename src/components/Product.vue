db56efd24b46fb261be33d233b45717bd39b20d0
Ismael Assunção Costa <maelcosta96@gmail.com>
2021-11-23 20:59:39 -0300

    Layout finalizado

---
+++ src/components/Product.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="card">
+    <img class="card__image" :src="require(`@/${imgSrc}`)"/>
+    <h1 class="card__title">{{title}}</h1>
+    <div class="card__price">${{price}}</div>
+    <div class="card__btn-group">
+      <button class="card__btn">Sell</button>
+      <input class="card__input" :value="quantity"/>
+      <button class="card__btn card__btn--primary">Buy</button>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.card {
+    max-width: 100%;
+    background-color: #fff;
+    text-align: center;
+    font-size: 18px;
+    padding: 20px 15px;
+    width: 100%;
+
+  .card__title {
+    font-size: 22px;
+    font-weight: 700;
+  }
+
+  .card__price {
+    font-size: 20px;
+    color: #24c486;
+  }
+
+  .card__image {
+    margin: 15px auto;
+    height: 120px;
+  }
+
+  .card__btn-group {
+    font-size: 20px;
+    font-weight: 700;
+    display: grid;
+    grid-template-columns: repeat(3,1fr);
+    grid-gap: 10px 10px;
+    margin-top: 25px;
+
+    .card__btn {
+      cursor: pointer;
+      padding: 10px 12px;
+      border-radius: 3px;
+      border: none;
+      font-weight: 700;
+    }
+
+    .card__btn--primary {
+      background: linear-gradient(180deg,#2ecc71,#1abc9c);
+      color: #fff;
+    }
+
+    .card__input {
+      font-size: 16px;
+      text-align: center;
+      padding: 9px 12px;
+      border-radius: 3px;
+      border: 1px solid #b2bec3;
+      color: #333;
+      font-family: Roboto,sans-serif;
+      width: 100%;
+    }
+  }
+}
+</style>
+<script>
+export default {
+  props: {
+    imgSrc: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+  },
+  data() {
+    return {
+
+    };
+  },
+};
+</script>
--- src/components/Spend.vue
+++ src/components/Spend.vue
@@ -7,9 +7,28 @@
 
     <main>
       <div class="box">
-        <input class="box__balance" v-mask="'$###,###,###,###'" :value="balance"/>
+        <input class="box__balance" v-mask="'$###,###,###,###'" :value="100000000000 - total"/>
+      </div>
+
+      <div class="products" >
+        <Product
+          v-for="product in products" :key="product.title"
+          :imgSrc="product.imgSrc"
+          :title="product.title"
+          :price="product.price"
+          :quantity="product.quantity"
+        />
       </div>
     </main>
+
+    <footer class="receipt">
+      <h1 class="receipt__title">Your Receipt</h1>
+      <div class="receipt__divider" />
+      <div class="receipt__total">
+        <span>TOTAL</span>
+        <input v-mask="'$###,###,###,###'" :value="total"/>
+      </div>
+    </footer>
   </div>
 </template>
 <style lang="scss" scoped>
@@ -54,12 +73,97 @@
       }
     }
   }
+
+  .products {
+    display: grid;
+    grid-gap: 10px 10px;
+    grid-template-columns: repeat(3,minmax(0,1fr));
+  }
+
+  @media only screen and (max-width: 900px) {
+    .products {
+      grid-template-columns: repeat(2,1fr);
+    }
+  }
+
+  @media only screen and (max-width: 600px) {
+    .products {
+      grid-template-columns: repeat(1,1fr);
+    }
+  }
+
+  .receipt {
+    max-width: 100%;
+    font-size: 18px;
+    padding: 15px 15px 30px;
+    margin: 10px 0;
+    background-color: #fff;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .receipt__title {
+      font-size: 28px;
+      padding: 15px 15px 25px;
+      font-weight: 700;
+    }
+
+    .receipt__divider {
+      height: 1px;
+      background-color: #000;
+      width: 300px;
+      margin-bottom: 15px;
+    }
+
+    .receipt__total {
+      display: flex;
+      width: 300px;
+      justify-content: space-between;
+      font-weight: bold;
+
+      & > :last-child {
+        color: #24c486;
+        border-width: 0;
+        text-align: end;
+        font-weight: inherit;
+        font-size: 1rem;
+
+        &:focus {
+          outline: none;
+          caret-color: transparent;
+        }
+      }
+    }
+  }
 </style>
 <script>
+import Product from './Product.vue';
+
 export default {
+  components: { Product },
   data() {
     return {
-      balance: 100000000000,
+      total: 50000,
+      products: [
+        {
+          imgSrc: 'assets/img/big-mac.jpg',
+          title: 'Big Mac',
+          price: 2,
+          quantity: 0,
+        },
+        {
+          imgSrc: 'assets/img/flip-flops.jpg',
+          title: 'Flip Flops',
+          price: 2,
+          quantity: 0,
+        },
+        {
+          imgSrc: 'assets/img/coca-cola-pack.jpg',
+          title: 'Coca-cola Pack',
+          price: 2,
+          quantity: 0,
+        },
+      ],
     };
   },
 };
