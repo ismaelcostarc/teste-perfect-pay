de98342ede3cc101b134971939973cbd19f1beda
Ismael Assunçao Costa <ismael.costa@a7participacoes.com>
2021-11-23 14:06:33 -0300

    mascara do saldo

--- src/components/Spend.vue
+++ src/components/Spend.vue
@@ -1,10 +1,16 @@
 <template>
-  <main>
+  <div>
     <header class="header">
       <img class="header__image" src="../assets/img/billgates.jpg"/>
       <h1 class="header__title">Spend Bill Gates' Money</h1>
     </header>
-  </main>
+
+    <main>
+      <div class="box">
+        <input class="box__balance" v-mask="'$###,###,###,###'" :value="balance"/>
+      </div>
+    </main>
+  </div>
 </template>
 <style lang="scss" scoped>
   .header {
@@ -18,6 +24,34 @@
     .header__image {
       border-radius: 50%;
       width: 125px;
+      margin: 10px auto 25px;
+    }
+
+    .header__title {
+      font-weight: 700;
+    }
+  }
+
+  .box {
+    background: linear-gradient(180deg,#2ecc71,#1abc9c);
+    padding: 20px;
+    margin: 10px 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    .box__balance {
+      font-size: 32px;
+      color: #fff;
+      font-weight: 700;
+      background: none;
+      border-width: 0;
+      text-align: center;
+
+      &:focus {
+        outline: none;
+        caret-color: transparent;
+      }
     }
   }
 </style>
@@ -25,7 +59,7 @@
 export default {
   data() {
     return {
-
+      balance: 100000000000,
     };
   },
 };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
+import VueTheMask from 'vue-the-mask';
 import App from './App.vue';
 import store from './store';
 import './assets/styles/global.scss';
 
-createApp(App).use(store).mount('#app');
+createApp(App).use(store).use(VueTheMask).mount('#app');
