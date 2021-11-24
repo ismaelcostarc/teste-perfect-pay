b17050daefad0f6cc0014986cf91ac33c91ddf2a
Ismael Assunção Costa <maelcosta96@gmail.com>
2021-11-23 22:25:36 -0300

    criacao da store

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,29 @@
 import { createStore } from 'vuex';
+import products from '../data/products';
+
+const inventory = [];
+
+for (let i = 0; i < 45; i += 1) {
+  inventory[i] = {
+    id: i,
+    quantity: 0,
+  };
+}
 
 export default createStore({
   state: {
+    total: 0,
+    inventory,
   },
   mutations: {
+    buy(state, id) {
+      state.inventory[id].quantity += 1;
+      state.total += products[id].price;
+    },
+    sell(state, id) {
+      state.inventory[id].quantity += 1;
+      state.total -= products[id].price;
+    },
   },
   actions: {
   },
